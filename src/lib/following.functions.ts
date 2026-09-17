8e8264384e86db525e89fc6299eb947c65d3be67
sbstumblerx-gif <sb.stumblerx@gmail.com>
2026-09-17 17:47:21 +0300

    Add followed official publication feed server function

--- src/lib/following.functions.ts
+++ src/lib/following.functions.ts
@@ -9,3 +9,7 @@ export const followingOverview = createServerFn({ method: "GET" })
 export const entityFollowStats = createServerFn({ method: "GET" })
   .inputValidator((d: unknown) => z.object({ entity_type: z.enum(["driver", "team"]), entity_slug: z.string().min(1) }).parse(d))
   .handler(async ({ data }) => (await import("./entity-follow-stats.server")).entityFollowStats(data.entity_type, data.entity_slug));
+
+export const followingPublications = createServerFn({ method: "GET" })
+  .middleware([requireSupabaseAuth])
+  .handler(async ({ context }) => (await import("./following-publications.server")).followingPublications(context.userId));
